9cdea4ba999add6dabb6dc5d9cf24d611351bb34
Mohammed6544 <naytwus@gmail.com>
2026-04-02 17:49:35 +0300

    ggs

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/pixelatsa/",
   server: {
     host: "::",
     port: 8080,
